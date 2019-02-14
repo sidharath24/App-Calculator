c104b09c4404fd8c077954f98e9ee3adcb8af55a
visualstudio <visualstudio@122-PC32>
2019-02-14 10:39:07 -0500

    New

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -12,42 +12,53 @@ namespace App1
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        TextView vnum1, vnum2, vresult;
+        EditText tnum1, tnum2, tresult;
+        Button bplus, bsub, bmul, bdiv;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            vnum1 = (TextView)FindViewById(Resource.Id.tvnum1);
+            vnum2 = (TextView)FindViewById(Resource.Id.tvnum2);
+            
+            vresult = (TextView)FindViewById(Resource.Id.tvresult);
+            tnum1 = (EditText)FindViewById(Resource.Id.etnum1);
+            tnum2 = (EditText)FindViewById(Resource.Id.etnum2);
+            tresult = (EditText)FindViewById(Resource.Id.etresult);
+            bplus = (Button)FindViewById(Resource.Id.btnPlus);
+            bsub = (Button)FindViewById(Resource.Id.btnSub);
+            bmul = (Button)FindViewById(Resource.Id.btnMul);
+            bdiv = (Button)FindViewById(Resource.Id.btnDiv);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            bplus.Click += delegate
             {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a + b;
+                tresult.Text = r.ToString();
+            };
+            bsub.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a - b;
+                tresult.Text = r.ToString();
+            };
+            bmul.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a * b;
+                tresult.Text = r.ToString();
+            };
+            bdiv.Click += delegate
+            {
+                double a = double.Parse(tnum1.Text);
+                double b = double.Parse(tnum2.Text);
+                double r = a / b;
+                tresult.Text = r.ToString();
+            };
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-	}
+    }
 }
-
